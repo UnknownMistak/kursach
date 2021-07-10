de282a055fd3f20927d78dcc9b15c2b82a305081
UnknownMistak <yurii.v.zaytsev@gmail.com>
2021-07-11 00:03:52 +0300

    last

--- portfolios/accs/portfolios/accs/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accs'
---
+++ readme.txt
@@ -0,0 +1,6 @@
+Чтобы запустить - python ./manage.py runserver
+
+запускается на localhost:8000/
+
+админка - localhost:8000/admin 
+лог/пасс - admin/admin
